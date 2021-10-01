abcfe0739b1a33b6bf448cc37cf2435bad547070
derekzx <chinzhixian@gmail.com>
2021-10-01 12:27:41 +0800

    Support 15 min time intervals

--- src/databaseClient.js
+++ src/databaseClient.js
@@ -1,13 +1,18 @@
-const mysql = require('mysql')
-const { createPool } = require('mysql');
+const mysql = require('mysql2')
 
 // Database Connection for Production
 
-const pool = mysql.createPool({
+const defaultPool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
-  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, // For production
+  // host: `${process.env.DB_HOST}`, // For local testing
+  connectionLimit: 20,
+  waitForConnections: true,
+  queueLimit: 5,
 });
+
+const pool = defaultPool.promise();
 
 module.exports = pool;
