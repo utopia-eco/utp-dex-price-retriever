1f603192a224be546ae7e2df926778d56ee78ca6
derekzx <chinzhixian@gmail.com>
2021-10-03 22:01:59 +0800

    cors

--- src/server.js
+++ src/server.js
@@ -7,6 +7,15 @@ const http = require('http');
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
   cors: {
     origin: '*',
     methods: ["GET", "POST"],
